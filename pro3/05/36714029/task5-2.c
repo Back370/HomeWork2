3413c8e88c767cd1404da7db38c41083e46dea6b
endo yuto <168190155+Back370@users.noreply.github.com>
2025-11-12 13:06:09 +0900

    a

--- pro3/05/36714029/task5-2.c
+++ pro3/05/36714029/task5-2.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-void gobai(int *a, int *b){
-    *a = *a * 5;
-    *b = *b * 5;
+struct input_data{
+    int x;
+    long y;
+    double z;
 };
-int main(void)
-{
-int a, b;
-a = 2;
-b = 3;
-gobai(&a, &b);
-printf("a=%d, b=%d",a,b );
-return (0);
+int main(void){
+    struct input_data data;
+
+    printf("int型、long型、double型の各変数のサイズを入力せよ。\n");
+    scanf("%d %ld %lf", &data.x, &data.y, &data.z);
+
+
+    printf("a = %d\n", data.x);
+    printf("b= %ld\n", data.y);
+    printf("c= %.1f\n", data.z);
+
+    return 0;
 }
+
