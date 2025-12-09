f74a11cf8b665654373ebb1b281453158c8a2d7f
endo yuto <168190155+Back370@users.noreply.github.com>
2025-12-09 09:22:20 +0900

    初期設定

---
+++ report_sample2/rep21b/ex3b/Move.java
@@ -0,0 +1,15 @@
+package report_sample2.rep21b.ex3b;
+
+public class Move {
+int removal;
+Color color;
+
+public Move(int removal, Color color) {
+this.removal = removal;
+this.color = color;
+}
+
+public String toString() {
+return String.format("took %d stone(s)", this.removal);
+}
+}
---
+++ report_sample2/rep21b/ex3b/RandomPlayer.java
@@ -0,0 +1,13 @@
+package report_sample2.rep21b.ex3b;
+
+public class RandomPlayer extends Player {
+public RandomPlayer() {
+super("Random");
+}
+
+Move search(State state) {
+var moves = state.getMoves();
+int index = new java.util.Random().nextInt(moves.size());
+return moves.get(index);
+}
+}
