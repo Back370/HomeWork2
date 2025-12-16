e9cad34071ede06b891c76d5667dddaa9045c2f0
endo yuto <168190155+Back370@users.noreply.github.com>
2025-12-16 09:13:30 +0900

    課題2-1セットアップ完了

---
+++ report_sample2/rep22a/ex4a/Move.java
@@ -0,0 +1,13 @@
+public class Move {
+	int index;
+	Color color;
+
+	public Move(int index, Color color) {
+		this.index = index;
+		this.color = color;
+	}
+
+	public Move flipped() {
+		return new Move(this.index, this.color.flipped());
+	}
+}
---
+++ report_sample2/rep22a/ex4a/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+	String name;
+	Color color;
+
+	public Player(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return this.name;
+	}
+
+	public Move think(State state) {
+		var move = search(state);
+		move.color = this.color;
+		return move;
+	}
+
+	Move search(State state) {
+		return null;
+	}
+}
