797fa898ae81e39e14853795a6d0ff70fef7a21c
endo yuto <168190155+Back370@users.noreply.github.com>
2025-12-18 14:12:35 +0900

    知能処理学一段落

--- pro3/12/36714029/task12-2.c
+++ pro3/12/36714029/task12-2.c
@@ -9,4 +9,18 @@ int main(void){
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
+    ip = (int *)malloc(n * sizeof(int));
+
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &ip[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        printf("%d ", ip[i]);
+        printf("\n");
+    }
+
+    free(ip);
+    return 0;
 }
